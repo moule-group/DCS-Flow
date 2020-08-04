aa7b5c14e8fa153db0f4e5eb3623be941db229f2
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2020-08-04 19:21:01 -0300

    adding vasp calculator

--- cnss/cnss/relax.py
+++ cnss/cnss/relax.py
@@ -1,7 +1,6 @@
 import os
 import glob
-from ase.calculators.dftb import Dftb
-from ase.optimize import QuasiNewton
+from ase.optimize import BFGS
 from ase.io import read
 from cnss import mkdir, chdir, out
 
@@ -12,6 +11,7 @@ class CLICommand:
     @staticmethod
     def add_arguments(parser):
         add = parser.add_argument
+        add('--calc', help='Calculator used. Options are dftbp or vasp', default='dftbp')
         add('--geo', help='Name of geometry file for structure (cif or gen extensions)')
         add('--krelax',
             help='Number of k points for relaxation, e.g., 6 6 6',
@@ -20,9 +20,10 @@ class CLICommand:
             type=int)
         add('--fmax', help='Convergence criteria for forces', default=0.01)
 
+
     @staticmethod
     def run(args):
-        relax(args.krelax, args.fmax, args.geo)
+        relax(args.krelax, args.fmax, args.geo, args.calc)
 
 def relax_done(fmax):
     import pandas as pd
@@ -38,24 +39,41 @@ def relax_done(fmax):
         return False
 
     
-def relax_structure(krelax, fmax, geo):
+def relax_structure(krelax, fmax, geo, calc):
     if relax_done(fmax = fmax):
         return
     else:
         atoms = read(geo)
         formula = atoms.get_chemical_formula()
-        atoms.set_calculator(Dftb(label=formula,
-                                  atoms=atoms,
-                                  kpts=krelax,
-                                  Hamiltonian_SCC='Yes',
-                                  Hamiltonian_MaxAngularMomentum_='',
-                                  Hamiltonian_MaxAngularMomentum_C='p',
-                                  Hamiltonian_MaxAngularMomentum_H='s'))
-        opt = QuasiNewton(atoms, trajectory= formula + '.traj')
+
+        if calc == 'dftbp':
+            from ase.calculators.dftb import Dftb
+            calc = Dftb(label=formula,
+                        atoms=atoms,
+                        kpts=krelax,
+                        Hamiltonian_SCC='Yes',
+                        Hamiltonian_MaxAngularMomentum_='',
+                        Hamiltonian_MaxAngularMomentum_C='p',
+                        Hamiltonian_MaxAngularMomentum_H='s'))
+        elif calc == 'vasp':
+            from ase.calculators.dftb import Vasp
+            calc = Vasp(kpts=krelax,
+                        encut=520,
+                        prec='Accurate',
+                        nwrite=1,
+                        lcharg=False,
+                        lwave=False,
+                        xc='optPBE-vdw',
+                        gamma=True)
+        else:
+            raise NotImplementedError('{} calculator not implemented' .format(calc))
+            
+        atoms.set_calculator(calc)
+        opt = BFGS(atoms, trajectory= formula + '.traj')
         opt.run(fmax=fmax)
 
         
-def relax(krelax=[6, 6, 6], fmax=0.01, geo=None):
+def relax(krelax=[6, 6, 6], fmax=0.01, geo=None, calc='dftbp'):
     folder = os.getcwd()
     if not geo:
         geo = glob.glob(folder + '/*.cif') + \
@@ -66,7 +84,7 @@ def relax(krelax=[6, 6, 6], fmax=0.01, geo=None):
     mkdir(folder + '/1-optimization')
     with chdir(folder + '/1-optimization'):
         with out('relax'):
-            relax_structure(krelax=krelax, fmax=fmax, geo=geo[0])
+            relax_structure(krelax=krelax, fmax=fmax, geo=geo[0], calc=calc)
 
 if __name__ == '__main__':
     relax()
